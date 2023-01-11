0fefa045a6cb6a4c320a0d3b09bed98c4bd3e3db
David Siwulec <david.siwulec@gmail.com>
2023-01-10 21:06:58 -0500

    Initial rich text formatting

--- react-app/src/components/NoteForm/NoteForm.js
+++ react-app/src/components/NoteForm/NoteForm.js
@@ -1,10 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import { useQuill } from 'react-quilljs';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateNote } from '../../store/note';
 import { getAllNotebooks } from '../../store/notebook';
+import 'quill/dist/quill.snow.css';
 import './NoteForm.css'
 
 const NoteForm = ({ id, title, setTitle, text, setText, titleCharCount, setTitleCharCount, notebookId, setNotebookId }) => {
+    const theme = 'snow';
+
+    const modules = {
+        toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            [{ align: [] }],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            [{ indent: '-1' }, { indent: '+1' }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            ['link'],
+            [{ color: [] }, { background: [] }],
+            ['clean'],
+        ],
+        clipboard: {
+            matchVisual: false,
+        },
+    };
+
+    const placeholder = 'Start writing so you never forget the important things...';
+
+    const formats = [
+        'bold', 'italic', 'underline', 'strike',
+        'align', 'list', 'indent', 'header',
+        'link', 'color', 'background', 'clean'
+    ];
+
+    const { quill, quillRef } = useQuill({ theme, modules, formats, placeholder });
+
     const [errors, setErrors] = useState([]);
     const dispatch = useDispatch();
 
@@ -29,6 +59,18 @@ const NoteForm = ({ id, title, setTitle, text, setText, titleCharCount, setTitle
             await dispatch(getAllNotebooks());
         })()
     }, [dispatch])
+
+    useEffect(() => {
+        if (quill) {
+            quill.on('text-change', (delta, oldDelta, source) => {
+                console.log('Text change!');
+                console.log(quill.getText()); // Get text only
+                console.log(quill.getContents()); // Get delta contents
+                console.log(quill.root.innerHTML); // Get innerHTML using quill
+                console.log(quillRef.current.firstChild.innerHTML); // Get innerHTML using quillRef
+            });
+        }
+    }, [quill]);
 
     const updateTitle = (e) => {
         setTitle(e.target.value);
@@ -57,7 +99,7 @@ const NoteForm = ({ id, title, setTitle, text, setText, titleCharCount, setTitle
                 />
                 <div className='char-count'>{titleCharCount}/35</div>
             </div>
-            <div id='note-text-input-container'>
+            <div id='note-text-input-container' ref={quillRef}>
                 <textarea
                     id='note-text-input'
                     name='text'
